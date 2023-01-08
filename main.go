98d66e3a20d8bf45ec5289ff668196dd67d54b2d
Lucas <lucas@MacBook-Pro-de-Lucas-2.local>
2023-01-08 23:31:00 +0100

    ADD- open file and create scanner in the main func and then call the parser

--- main.go
+++ main.go
@@ -2,57 +2,23 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"log"
 	"os"
-	"strconv"
-	"strings"
+	"projectGo/src"
+	"projectGo/src/errors"
 )
 
-type Pos struct {
-	X, Y int
-}
-
-type Map struct {
-	X int
-	Y int
-}
-
-type error interface {
-	Error() string
-}
-
-func firstLine(firstLine []string, entrepot Map) {
-	entrepot.X, err = strconv.Atoi(firstLine[0])
-	entrepot.Y, err = strconv.Atoi(firstLine[1])
-	if err != nil {
-		// ... handle error
-		panic(err)
-	}
-	fmt.Println(entrepot.X)
-}
-
-func fileParser(lines []string, entrepot Map) {
-	firstLine(strings.Split(lines[0], " "), entrepot)
-}
-
-// open a file and read it line by line and print it to the console
 func main() {
-	entrepot := Map{0, 0}
-	fileToParse := os.Args[1]
-	file, err := os.Open(fileToParse)
-	if err != nil {
-		log.Fatal(err)
+	// Open the file to read
+	file, err := os.Open("file.txt")
+	ret := errors.Check(err)
+	if ret == 1 {
+		return
 	}
 	defer file.Close()
 
-	var lines []string
+	// Create a scanner to scan the file line by line
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	fileParser(lines, entrepot)
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	scanner.Scan()
+
+	_, _, _, _ = src.Parser(file, scanner)
 }
