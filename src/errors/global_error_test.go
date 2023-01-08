e4a37fc0c18e858af989ae4ac953f1d47a72582a
Lucas <lucas@MacBook-Pro-de-Lucas-2.local>
2023-01-09 00:31:08 +0100

    ADD- errors tests about global and scanner

---
+++ src/errors/global_error_test.go
@@ -0,0 +1,21 @@
+package errors
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCheck(t *testing.T) {
+	// Test error input
+	err := fmt.Errorf("test error")
+	ret := Check(err)
+	if ret != 1 {
+		t.Errorf("Expected return value to be 1, got %d", ret)
+	}
+
+	// Test non-error input
+	ret = Check(nil)
+	if ret != 0 {
+		t.Errorf("Expected return value to be 0, got %d", ret)
+	}
+}
