133cd6ec1cafa0d1cc28aebe4fe8a7a44ef219a5
Lucas <lucas@MacBook-Pro-de-Lucas-2.local>
2023-01-09 00:17:17 +0100

    ADD- CreateTruck function to create an object named Truck

---
+++ src/components/truck.go
@@ -0,0 +1,31 @@
+package components
+
+import (
+	"projectGo/src/errors"
+	"strconv"
+)
+
+type Truck struct {
+	Name         string
+	X, Y         int
+	MaxLoad      int
+	Availability int
+}
+
+func CreateTruck(parts []string, trucks []Truck) []Truck {
+	ret := 0
+	x, err := strconv.Atoi(parts[1])
+	ret += errors.Check(err)
+	y, err := strconv.Atoi(parts[2])
+	ret += errors.Check(err)
+	maxLoad, err := strconv.Atoi(parts[3])
+	ret += errors.Check(err)
+	availability, err := strconv.Atoi(parts[4])
+	ret += errors.Check(err)
+	if ret != 0 {
+		return trucks
+	}
+	t := Truck{Name: parts[0], X: x, Y: y, MaxLoad: maxLoad, Availability: availability}
+	trucks = append(trucks, t)
+	return trucks
+}
