3032c1bc0ea0e884e0d6a2aa42c2b1be9b276fce
Lucas <lucas@MacBook-Pro-de-Lucas-2.local>
2023-01-09 00:14:02 +0100

    ADD- Parse the first row (number of squares in width, number of squares in length, number of turns)

---
+++ src/components/first_line.go
@@ -0,0 +1,31 @@
+package components
+
+import (
+	"bufio"
+	"fmt"
+	"projectGo/src/errors"
+	"strconv"
+	"strings"
+)
+
+func ParseFirstLine(scanner *bufio.Scanner) (int, int, int, int) {
+	line := scanner.Text()
+	parts := strings.Split(line, " ")
+	ret := 0
+	fmt.Print(parts)
+	if len(parts) != 3 {
+		fmt.Println("Erreur de format: première ligne doit avoir 3 éléments")
+		ret = 1
+		return 0, 0, 0, ret
+	}
+	width, err := strconv.Atoi(parts[0])
+	ret += errors.Check(err)
+	height, err := strconv.Atoi(parts[1])
+	ret += errors.Check(err)
+	turns, err := strconv.Atoi(parts[2])
+	ret += errors.Check(err)
+	if ret != 0 {
+		return 0, 0, 0, ret
+	}
+	return width, height, turns, ret
+}
