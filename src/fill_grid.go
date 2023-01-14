23465506c7e06aad26b05112428ea0140a79e654
Lucas <lucas@MacBook-Pro-de-Lucas-2.local>
2023-01-14 03:57:35 +0100

    ADD- comments and -1 case

--- src/fill_grid.go
+++ src/fill_grid.go
@@ -1,29 +1,37 @@
+// Package src provides main function on src files
 package src
 
 import (
 	"projectGo/src/components"
 )
 
+// FillGrid func to fill the grid
 func FillGrid(w *components.Warehouse) *components.Warehouse {
 	// Initialize all squares in the grid as empty
 	for i := 0; i < w.Height; i++ {
 		for j := 0; j < w.Width; j++ {
-			w.Grid[i][j] = 0
+			if w.Grid[i][j] != -1 {
+				w.Grid[i][j] = 0
+			}
 		}
 	}
 
 	// Fill the grid with parcels
 	for _, parcel := range w.Parcels {
-		w.Grid[parcel.Y][parcel.X] = 1
+		if w.Grid[parcel.Y][parcel.X] != -1 {
+			w.Grid[parcel.Y][parcel.X] = 1
+		}
 	}
 
-	// Fill the grid with pallets
-	for _, pallet := range w.Pallets {
+	// Fill the grid with PalletTruck
+	for i := range w.PalletTruck {
+		pallet := w.PalletTruck[i]
 		w.Grid[pallet.Y][pallet.X] = 2
 	}
 
 	// Fill the grid with trucks
-	for _, truck := range w.Trucks {
+	for i := range w.Trucks {
+		truck := w.Trucks[i]
 		w.Grid[truck.Y][truck.X] = 3
 	}
 	return w
--- src/print_grid.go
+++ src/print_grid.go
@@ -1,3 +1,4 @@
+// Package src provides main function on src files
 package src
 
 import (
@@ -5,10 +6,13 @@ import (
 	"projectGo/src/components"
 )
 
+// PrintGrid func to print the grid
 func PrintGrid(w *components.Warehouse) {
 	for i := 0; i < w.Height; i++ {
 		for j := 0; j < w.Width; j++ {
 			switch w.Grid[i][j] {
+			case -1:
+				fmt.Print("X")
 			case 0:
 				fmt.Print(".")
 			case 1:
